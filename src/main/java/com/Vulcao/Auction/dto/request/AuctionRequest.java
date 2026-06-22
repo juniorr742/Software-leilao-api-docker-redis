d56824b87c5a3295bfe3e437a0c7553c9e958315
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-06-22 10:00:06 -0300

    "refactory: removendo atributo id de requisição"

--- src/main/java/com/Vulcao/Auction/dto/request/AuctionRequest.java
+++ src/main/java/com/Vulcao/Auction/dto/request/AuctionRequest.java
@@ -1,13 +1,16 @@
 package com.Vulcao.Auction.dto.request;
 
+
 import com.Vulcao.Auction.model.enums.AuctionStatus;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record AuctionRequest(UUID idAuction,
-                             UUID idProduct,
-                             LocalDateTime startAction,
-                             LocalDateTime finishAction,
-                             AuctionStatus status) {
+public record AuctionRequest(
+        UUID idProduct,
+        BigDecimal actualPrice,
+        LocalDateTime startAction,
+        LocalDateTime finishAction,
+        AuctionStatus status) {
 }
--- src/main/java/com/Vulcao/Auction/dto/request/BidRequest.java
+++ src/main/java/com/Vulcao/Auction/dto/request/BidRequest.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record BidRequest(UUID idUser,
+                         UUID idAuction,
                          BigDecimal value,
                          LocalDateTime timestamp) {
 }
