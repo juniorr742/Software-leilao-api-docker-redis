843cb81d8731f0a2d9efe4bbd66091460c06d15b
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-06-28 13:41:35 -0300

    "feat: adicionando método de busca para atualizações de cache"

--- src/main/java/com/Vulcao/Auction/repositorys/IBidRepository.java
+++ src/main/java/com/Vulcao/Auction/repositorys/IBidRepository.java
@@ -2,14 +2,20 @@ package com.Vulcao.Auction.repositorys;
 
 import com.Vulcao.Auction.model.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface IBidRepository extends JpaRepository<Bid, UUID> {
 
     List<Bid> findByUser_IdUsuario(UUID idUsuario);
     List<Bid> findByAuction_IdAuction(UUID idAuction);
 
+    Optional<Bid> findFirstByAuction_IdAuctionAndUser_IdUsuarioAndValue(UUID idAuction, UUID idUsuario, BigDecimal value);
+
+    Optional<Bid> findTopByAuction_IdAuctionOrderByValueDesc(UUID idAuction);
 }
