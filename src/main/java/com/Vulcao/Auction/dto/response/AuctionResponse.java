65953fb9373a5fa5242ab5626ef7a34911a422f5
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-07-05 13:27:22 -0300

    "refactory: trocando tipo de dado do response"

--- src/main/java/com/Vulcao/Auction/dto/response/AuctionResponse.java
+++ src/main/java/com/Vulcao/Auction/dto/response/AuctionResponse.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 public record AuctionResponse(
         UUID id,
-        ProductResponse product,
+        UUID idProduct,
         BigDecimal price,
         LocalDateTime startAction,
         LocalDateTime finishAction,
