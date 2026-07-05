1761faa9c03e0606eeef86bdff2c80171ce5f0dc
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-07-05 13:25:21 -0300

    "refactory: trocando tipo de retorno de busca do repositório"

--- src/main/java/com/Vulcao/Auction/repositorys/IAuctionRepository.java
+++ src/main/java/com/Vulcao/Auction/repositorys/IAuctionRepository.java
@@ -3,11 +3,12 @@ package com.Vulcao.Auction.repositorys;
 import com.Vulcao.Auction.model.Auction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface IAuctionRepository extends JpaRepository<Auction, UUID> {
     Optional<Auction> findByProduct_IdProduct(UUID idProduct);
 
-    Optional<Auction> findByProduct_Owner_IdUsuario(UUID idUsuario);
+    List<Auction> findByProduct_Owner_IdUsuario(UUID idUsuario);
 }
