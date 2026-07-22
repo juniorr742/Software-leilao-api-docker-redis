8ef4b5de37d0807e5b01d12a8c4a3fe0b341b735
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-07-22 09:06:07 -0300

    "ref: adicionando uma query de busca por leilões que o horário de finalização ja passou"

--- src/main/java/com/Vulcao/Auction/repositorys/IAuctionRepository.java
+++ src/main/java/com/Vulcao/Auction/repositorys/IAuctionRepository.java
@@ -1,8 +1,12 @@
 package com.Vulcao.Auction.repositorys;
 
 import com.Vulcao.Auction.model.Auction;
+import com.Vulcao.Auction.model.enums.AuctionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -11,4 +15,7 @@ public interface IAuctionRepository extends JpaRepository<Auction, UUID> {
     Optional<Auction> findByProduct_IdProduct(UUID idProduct);
 
     List<Auction> findByProduct_Owner_IdUsuario(UUID idUsuario);
+
+    @Query("SELECT a FROM Auction a WHERE a.status = :status AND a.finishAction <= :now")
+    List<Auction> findAuctionToFinalize(@Param("now") LocalDateTime now, @Param("status") AuctionStatus status);
 }
