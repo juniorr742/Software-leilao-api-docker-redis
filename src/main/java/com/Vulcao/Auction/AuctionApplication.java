eec39b378b7042c073b18452920767d5166a1879
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-07-22 09:07:40 -0300

    "ref: adicionando Scheduling para verificar toda hora leilões que ja foram finalizados"

--- src/main/java/com/Vulcao/Auction/AuctionApplication.java
+++ src/main/java/com/Vulcao/Auction/AuctionApplication.java
@@ -3,9 +3,11 @@ package com.Vulcao.Auction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class AuctionApplication {
 
 	public static void main(String[] args) {
