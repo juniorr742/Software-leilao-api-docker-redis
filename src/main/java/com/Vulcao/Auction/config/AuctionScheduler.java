3099b1bb1a1cb4d8b90d2968b13ba2164b1d0ba6
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-07-22 09:08:54 -0300

    "feat: adicionando método de finalização de leilões por Scheduled"

---
+++ src/main/java/com/Vulcao/Auction/config/AuctionScheduler.java
@@ -0,0 +1,38 @@
+package com.Vulcao.Auction.config;
+
+import com.Vulcao.Auction.model.Auction;
+import com.Vulcao.Auction.model.enums.AuctionStatus;
+import com.Vulcao.Auction.repositorys.IAuctionRepository;
+import com.Vulcao.Auction.service.AuctionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class AuctionScheduler {
+
+    @Autowired
+    private IAuctionRepository auctionRepository;
+
+    @Autowired
+    private AuctionService auctionService;
+
+    @Scheduled(fixedDelay = 10000)
+    public void finishAuction(){
+        LocalDateTime agora = LocalDateTime.now();
+
+        List<Auction> finish = auctionRepository.findAuctionToFinalize(agora, AuctionStatus.ATIVO);
+
+        if (!finish.isEmpty()){
+            log.info("Encontrados {} leilões para finalizar...", finish.size());
+
+            auctionService.finishAuctions(finish);
+        }
+    }
+}
